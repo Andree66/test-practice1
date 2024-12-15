f6b4a05eb8c2798053e7c3b00c8775f7a2ab2b63
Andrii.S <iron.appp@gmai.com>
2024-12-15 11:42:15 +0200

    init commit

---
+++ test1.js
@@ -0,0 +1,5 @@
+const longString = "jhjhj3hj2h3r32jrhrj2rh2jkrh2jkrh2jrkh2jkrh23jkrh2jrhj23hrjk32rh23kjrh2kjh32rkj23jkrh2jrkh2jkrh23jkrh2jrhj23hrjk32rh23kjrh2kjh32rkj23jkrh2jrkh2jkrh23jkrh2jrhj23hrjk32rh23kjrh2kjh32rkj23"
+let userName = "Test"
+const object={
+    userName:"TestProperty"
+}
